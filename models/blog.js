2cc04c7b646838bc4788ee4527ee709859e9ebf8
Isaias <isaiasjre2511@gmail.com>
2021-12-20 12:42:40 -0500

    Blog model modified, code formatted

--- models/blog.js
+++ models/blog.js
@@ -1,36 +1,34 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const blogScheme = new mongoose.Schema(
-    {
-        title:{
-            type: String,
-            required : true,
-            unique : [true, 'Ya existe un articulo con este nombre']
-        },
-        user: {
-            type: String,
-            required: true
-        },
-        content: {
-            type: String,
-            // required: true,
-        },
-        img : {
-            type: String
-        },
-        descripcion: {
-            type: String
-        },
-        date: {
-            type: Date,
-            default: Date.now()
-        }
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    {
-        versionKey : false,
-        timestamps: true,
-    }
-)
+    user: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      // required: true,
+    },
+    img: {
+      type: String,
+    },
+    descripcion: {
+      type: String,
+    },
+    date: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('blog', blogScheme)
+module.exports = mongoose.model("blog", blogScheme);
